dc2d184a44da9e867ad4d420087776772d688320
Marco Asus Laptop <marcovillegas.g@gmail.com>
2024-02-08 16:10:57 -0300

    first commitment

---
+++ heic2jpg.py
@@ -0,0 +1,24 @@
+""" HEIC to jpg
+ref: https://www.youtube.com/watch?v=7Bc3uDi0WHw
+"""
+
+import os
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+
+input_folder = r"C:\Users\marco\Imágenes\FOTOS CONGRESO\nancy_HEIC"
+output_folder = r"C:\Users\marco\Imágenes\FOTOS CONGRESO\nancy_jpg"
+files = os.listdir(input_folder)
+
+register_heif_opener()
+
+# Convert each file to JPEG
+for i, photo in enumerate(files):
+    counter = f"({i+1}/" + str(len(files)) + ")"
+    print(counter + f" Converting {photo} ... ", end=" ")
+
+    temp_img = Image.open(os.path.join(input_folder, photo))
+    temp_img.save(os.path.join(output_folder, photo.replace(".HEIC", ".jpg")))
+
+    print("done")
